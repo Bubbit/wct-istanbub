510ce73fae05124d132754b8416c6fa4469075da
Westbrook Johnson <westbrook.johnson@gmail.com>
2018-07-16 08:36:13 -0400

    Update yarn for new versions of `istanbul-*` dependencies

--- test/mocks/mockJS.js
+++ test/mocks/mockJS.js
@@ -25,6 +25,10 @@ class MyView1 extends PolymerElement {
       <div class="card">
         <div class="circle">1</div>
         <h1>View One</h1>
+        <img src="${new URL(
+          'local-image.jpg',
+          import.meta.url
+        )}" />
         <p>Ut labores minimum atomorum pro. Laudem tibique ut has.</p>
         <p>Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Lorem ipsum dolor sit amet, per in nusquam nominavi periculis, sit elit oportere ea.Cu mei vide viris gloriatur, at populo eripuit sit.</p>
       </div>
